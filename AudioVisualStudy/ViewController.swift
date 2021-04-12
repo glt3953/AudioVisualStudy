204b12272b41e38496978da44090aad640e876c2
guoliting <guoliting@didichuxing.com>
2021-04-12 16:41:59 +0800

    实现URL视频播放

--- AudioVisualStudy/ViewController.swift
+++ AudioVisualStudy/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var playVideo: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func playVideo(_ sender: UIButton) {
+        guard let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else {
+            return
+        }
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
 
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
 }
 
